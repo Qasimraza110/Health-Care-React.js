614db931e081e8ea9cf7361709e2d45d32cf4c36
Qasim Raza <sh.qasim.raza110@gmail.com>
2025-10-18 01:05:16 -0700

    Fix import statement for Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Home"; 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
